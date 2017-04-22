6f655b17483a02281bc570fdfd8bcb1abd4c87fd
Mathieu Zhang <mathfeel+voxels@gmail.com>
2017-04-21 17:42:52 -0700

    Break up steps of `dice_coef` to make them more readable.

--- unet/utils.py
+++ unet/utils.py
@@ -18,8 +18,7 @@ def hist_summary(folds_history):
         res = res.append(res.mean,ignore_index=True)
     return res
 
-
-def clean_segmentation_img(img,cut=200):
+def clean_segmentation_img(img, cut=200):
     seg_binary = np.zeros_like(img)
     _,sb = cv2.threshold(np.copy(img)*255, 127, 255, cv2.THRESH_BINARY)
     im = sb.astype(np.uint8)
@@ -40,20 +39,35 @@ def clean_segmentation_img(img,cut=200):
     seg_binary[cimg == 255] = 1
     return seg_binary
 
-def dice_coef(y_true, y_pred, smooth, pred_mul, p_ave):
+def dice_coef(y_true, y_pred, smooth, pred_mul, p_ave, negate=False):
+    '''Return the dice coefficent between {y_true} and {y_pred}
+    Normally defined as:
+        dice = 2*sum(y_true && y_pred) / sum(y_true || y_pred)
+    where the sum is over all samples
+
+    Additionally can be turned by parameters smooth, pred_mul:
+        pred_mul : multiplicative factor to predict
+        smooth   : smooth out division by zero when y~0?
+        dice' = (2*sum(y_true&&y_pred*pred_mul)+smooth)/(sum(y_true||y_pred*pred_mul)+smooth)
+    '''
     #y_true_f = K.flatten(y_true)
     #y_pred_f = K.flatten(y_pred)
     ### this method: combine all as a single big image
     #intersection = K.sum(y_true_f * y_pred_f)
     #return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
     #each image has its own score
-    intersection = K.sum(y_true*y_pred,axis=[1,2,3])
-    union = K.sum(y_true, axis=[1,2,3])+K.sum(y_pred,axis=[1,2,3])*pred_mul
+    isect_avg = K.sum(y_true*y_pred,axis=[1,2,3])
+    union_avg = K.sum(y_true, axis=[1,2,3])+K.sum(y_pred,axis=[1,2,3])*pred_mul
     #return K.mean((2. * intersection + smooth) / (union + smooth))
 
-    intersectionA = K.sum(y_true*y_pred)
-    unionA = K.sum(y_true)+K.sum(y_pred)*pred_mul
-    return (1.0-p_ave)*K.mean((2. * intersection + smooth) / (union + smooth)) + p_ave*(2.0*intersectionA+smooth)/(unionA+smooth)
+    isect_all = K.sum(y_true*y_pred)
+    union_all = K.sum(y_true)+K.sum(y_pred)*pred_mul
+
+    dice_avg = K.mean((2.0*isect_avg+smooth)/(union_avg+smooth))
+    dice_all =        (2.0*isect_all+smooth)/(union_all+smooth)
+    # mix the two by p_ave
+    dice_mix = (1.-p_ave) * dice_avg + p_ave * dice_all
+    return -dice_mix if negate else dice_mix
 
 def dice_coef_loss_gen(smooth=10, pred_mul=1.0, p_ave=0.6):
     '''Generate dice_coef loss function by partially binding arguments'''
