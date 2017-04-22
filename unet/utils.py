cbb07b14725aa0924a195b23efc0820e0b08dfa5
Mathieu Zhang <mathfeel+voxels@gmail.com>
2017-04-21 17:43:15 -0700

    Add some JSON helper utilities

--- unet/utils.py
+++ unet/utils.py
@@ -1,6 +1,14 @@
+import functools
+import simplejson as json
+
 import pandas as pd
 import numpy as np
 from keras import backend as K
+
+# debugging
+#from pprint import pprint
+#from ipdb import set_trace
+
 
 def hist_summary(folds_history):
     res = None
@@ -75,3 +83,92 @@ def dice_coef_loss_gen(smooth=10, pred_mul=1.0, p_ave=0.6):
         '''loss function'''
         return -dice_coef(y_true, y_pred, smooth, pred_mul, p_ave)
     return loss
+
+####### Generic utils #######
+class dotdict(dict):
+    """ A dictionary whose attributes are accessible by dot notation.
+    This is a variation on the classic `Bunch` recipe (which is more limited
+    and doesn't give you all of dict's methods). It is just like a dictionary,
+    but its attributes are accessible by dot notation in addition to regular
+    `dict['attribute']` notation. It also has all of dict's methods.
+    .. doctest::
+        >>> dd = dotdict(foo="foofoofoo", bar="barbarbar")
+        >>> dd.foo
+        'foofoofoo'
+        >>> dd.foo == dd['foo']
+        True
+        >>> dd.bar
+        'barbarbar'
+        >>> dd.baz
+        >>> dd.qux = 'quxquxqux'
+        >>> dd.qux
+        'quxquxqux'
+        >>> dd['qux']
+        'quxquxqux'
+    NOTE:   There are a few limitations, but they're easy to avoid
+            (these should be familiar to JavaScript users):
+        1.  Avoid trying to set attributes whose names are dictionary methods,
+            for example 'keys'. Things will get clobbered. No good.
+        2.  You can store an item in a dictionary with the key '1' (`foo['1']`),
+            but you will not be able to access that attribute using dotted
+            notation if its key is not a valid Python variable/attribute name
+            (`foo.1` is not valid Python syntax).
+    FOR MORE INFORMATION ON HOW THIS WORKS, SEE:
+    - http://stackoverflow.com/questions/224026/
+    - http://stackoverflow.com/questions/35988/
+    """
+    def __getattr__(self, attr):
+        if attr in self:
+            return self[attr]
+        else:
+            raise AttributeError(attr)
+
+    __setattr__ = dict.__setitem__
+
+    __delattr__ = dict.__delitem__
+
+    @classmethod
+    def recursiveFrom(cls, d):
+        '''Generate instance recursively from dict {d}'''
+        return cls({
+            k : cls.recursiveFrom(v) if isinstance(v, dict) else v
+            for k, v in d.iteritems()})
+
+def get_json_type(obj):
+    """Serialize any object to a JSON-serializable structure.
+    # Arguments
+        obj: the object to serialize
+    # Returns
+        JSON-serializable structure representing `obj`.
+    # Raises
+        TypeError: if `obj` cannot be serialized.
+    """
+    # if obj is a serializable Keras class instance
+    # e.g. optimizer, layer
+    if hasattr(obj, 'get_config'):
+        return {'class_name': obj.__class__.__name__,
+                'config': obj.get_config()}
+
+    # if obj is any numpy type
+    if type(obj).__module__ == np.__name__:
+        return obj.item()
+
+    # misc functions (e.g. loss function)
+    if callable(obj):
+        return obj.__name__
+
+    # if obj is a python 'type'
+    if type(obj).__name__ == type.__name__:
+        return obj.__name__
+
+    raise TypeError('Not JSON Serializable:', obj)
+
+jsondumps = functools.partial(json.dumps, indent=1, sort_keys=True, separators=(',',':'), default=get_json_type)
+jsondump = functools.partial(json.dump, indent=1, sort_keys=True, separators=(',',':'), default=get_json_type)
+
+def safejsondump(j, f, *args, **kwds):
+    '''Dump json serializable object {j} to file {f}, break on exception so that we don't end up have partially written file without review'''
+    jsondumps(j)
+    jsondump(j, f, *args, **kwds)
+
+# eof
